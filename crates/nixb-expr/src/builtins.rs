6b42774fb3498635eb7731928ffb8008091f56b7
Riccardo Mazzarini <me@noib3.dev>
2026-06-20 17:43:33 +0200

    feat(expr): add more functionality to `Builtins`

--- crates/nixb-expr/src/builtins.rs
+++ crates/nixb-expr/src/builtins.rs
@@ -1,5 +1,7 @@
 //! TODO: docs.
 
+use alloc::ffi::CString;
+
 use crate::attrset::NixAttrset;
 use crate::callable::NixLambda;
 use crate::context::Context;
@@ -11,6 +13,33 @@ pub struct Builtins<'eval> {
 }
 
 impl<'eval> Builtins<'eval> {
+    /// Returns a handle to the `builtins.currentSystem` string.
+    #[inline]
+    pub fn current_system(&self, ctx: &mut Context) -> CString {
+        self.inner
+            .get(c"currentSystem", ctx)
+            .expect("builtins.currentSystem exists and it's a string")
+    }
+
+    /// Returns a handle to the `builtins.derivation` function.
+    #[inline]
+    pub fn derivation(&self, ctx: &mut Context) -> NixLambda<Borrowed<'eval>> {
+        self.inner
+            .get(c"derivation", ctx)
+            .expect("builtins.derivation exists and it's a function")
+    }
+
+    /// Returns a handle to the `builtins.derivationStrict` function.
+    #[inline]
+    pub fn derivation_strict(
+        &self,
+        ctx: &mut Context,
+    ) -> NixLambda<Borrowed<'eval>> {
+        self.inner
+            .get(c"derivationStrict", ctx)
+            .expect("builtins.derivationStrict exists and it's a function")
+    }
+
     /// Returns a handle to the `builtins.fetchGit` function.
     #[inline]
     pub fn fetch_git(&self, ctx: &mut Context) -> NixLambda<Borrowed<'eval>> {
@@ -38,5 +67,12 @@ impl<'eval> Builtins<'eval> {
     #[inline]
     pub(crate) fn new(inner: NixAttrset<Borrowed<'eval>>) -> Self {
         Self { inner }
+    }
+}
+
+impl<'eval> From<Builtins<'eval>> for NixAttrset<Borrowed<'eval>> {
+    #[inline]
+    fn from(builtins: Builtins<'eval>) -> Self {
+        builtins.inner
     }
 }
